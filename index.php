9be709b28282d1ad034d6f41ba624913fb6376ad
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-09 09:37:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $hautDeFrance = array(
+        'Aisne' => 02,
+        'Nord' => 59,
+        'Oise' => 60,
+        'Pas-de-Calais' => 62,
+        'Somme' => 80);
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>partie 5 exercice 5</title>
+    </head>
+    <body>
+    </body>
+</html>
